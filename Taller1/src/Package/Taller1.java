f496c7aed5a47bd411b53c35e06a3fbae286d672
mateo <mateo@192.168.1.102>
2026-03-24 22:29:11 -0300

    Avance con la lectura de archivo ademas con un poco de los menus

--- Taller1/src/Package/Taller1.java
+++ Taller1/src/Package/Taller1.java
@@ -11,30 +11,128 @@ import java.util.Scanner;
 
 public class Taller1 {
 
-	public static void main(String[] args) throws IOException {
-		File file1 = new File("Usuario.txt");
-		Scanner scanner1 = new Scanner(file1);
-
-		while(scanner1.hasNextLine()) {
-			String linea1 = scanner1.nextLine();
-			String[] partes1 = linea1.split(";");
-			String ID1 = partes1[0];
-			String Contraseña = partes1[1];
-			System.out.println(ID1);
-			System.out.println(Contraseña);
-			
-		}
-		File file2 = new File("Registros.txt");
-		Scanner scanner2 = new Scanner(file2);
+	public static void main(String[] args) {
+		Scanner lector1 = null;
+		Scanner lector2 = null;
 		
-		while(scanner2.hasNextLine()) {
-			String linea2 = scanner2.nextLine();
-			String[] partes2 = linea2.split(";");
-			String ID2 = partes2[0];
-			String Fecha = partes2[1];
-			
+		String[] Usuarios = leerArchivos1("Usuario.txt",lector1);
+		String[] Registros = leerArchivos2("Registros.txt",lector2);
+		
+		for(int i = 0; i < Usuarios.length; i++) {
+			if(Usuarios[i] != null) {
+				System.out.println(Usuarios[i]);
+			}
 		}
+		for(int j = 0; j < Registros.length; j++ ) {
+			if(Registros[j] != null) {
+				System.out.println(Registros[j]);
+			}
+		}
+
+		// aqui empieza el menu
+		Scanner scanner = new Scanner(System.in);
+		boolean inicio = false;
+
+		do {
+			System.out.println("1) Menu de Usuarios");
+			System.out.println("2) Menu de Analisis");
+			System.out.println("3) Salir");
+			
+			int comando = Integer.valueOf(scanner.nextLine());
+			
+			if(comando == 1) {
+				System.out.println("Usuario:");
+				String User = String.valueOf(scanner.nextLine());
+				
+				for(int i = 0; i < Usuarios.length; i++) {
+					if(Usuarios[i] != null) {
+						String[] partes1 = Usuarios[i].split(";");
+						String ID1 = partes1[0];
+						String Contraseña = partes1[1];
+						
+						if(ID1.equals(User)) {
+							System.out.println("Contraseña:");
+							String Password = String.valueOf(scanner.nextLine());
+							
+							if(Contraseña.equals(Password)) {
+								for(int j = 0; i < Registros.length; j++) {
+									if(Registros[j] !=null) {
+										String[] partes2 = Registros[j].split(";");
+										String ID2 = partes2[0];         
+										String Fecha = partes2[1];
+										String Horas = partes2[2];
+										String Actividad = partes2[3];
+										
+									}
+									
+								}
+								
+									
+								
+								Scanner scanner2 = new Scanner(System.in);
+								System.out.println("Acceso correcto!");
+								System.out.println("");
+								System.out.println("Bienvenido" + User+"!");
+								System.out.println("");
+								System.out.println("Que deseas realizar?");
+								System.out.println("");
+								System.out.println("1) Registrar actividad.");
+								System.out.println("2) Modificar actividad.");
+								System.out.println("3) Eliminar actividad.");
+								System.out.println("4) Cambiar contraseña");
+								System.out.println("5) Salir.");
+								
+								String opcion = String.valueOf(scanner.nextLine());
+								
+							}
+							
+						}
+					}
+				}
+			}
+
+			if(comando == 3) {
+				inicio = true; 
+			}
+			
+		} while(!inicio);
 	}
 
+	//lectura de archivos y sus partes establecidas
+	public static String[] leerArchivos1(String nombreArchivo1,Scanner lector1) {
+		String [] archivo1 = new String[300];
+		try {
+			File arch = new File(nombreArchivo1);
+			lector1 = new Scanner(arch);
+			int i = 0;
+			while(lector1.hasNextLine()) {
+				archivo1[i] = lector1.nextLine();
+				String[] partes1 = archivo1[i].split(";");
+				String ID1 = partes1[0];         
+				String Contraseña = partes1[1];  
+				i++;
+			}
+		}catch(IOException e) {}
+		return archivo1;
+	}
+
+	public static String[] leerArchivos2(String nombreArchivo2,Scanner lector2) {
+		String [] archivo2 = new String[300];
+		try {
+			File arch = new File(nombreArchivo2);
+			lector2 = new Scanner(arch);
+			int k = 0;
+			while(lector2.hasNextLine()) {
+				archivo2[k] = lector2.nextLine();
+				String[] partes2 = archivo2[k].split(";");
+				String ID2 = partes2[0];         
+				String Fecha = partes2[1];
+				String Horas = partes2[2];
+				String Actividad = partes2[3];
+				k++;
+			}
+		}catch(IOException e) {}
+		return archivo2;
+	}
 }
 
