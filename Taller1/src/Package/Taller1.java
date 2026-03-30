c88498c484c191f71bbc65d8974c19de866dc443
mateo <mateo@192.168.1.102>
2026-03-29 23:24:52 -0300

    Implemente metodos de modificar fecha de actividad de usuario
    corregí algunas fallas que tenia el ciclo for al recorrer la lista de
    usuarios
    y un gran avance en los menú de acceso como usuario
    y agregue un ciclo while que me faltaba

--- Taller1/src/Package/Taller1.java
+++ Taller1/src/Package/Taller1.java
@@ -1,11 +1,13 @@
 //Nombre: Mateo Arturo Godoy Carrasco
 //Rut: 21.924.663-3
 //Carrera: Ingenieria Civil Industrial
-//Link repositorio:
+//Link repositorio: https://github.com/mateogodoy07/Taller1.git
 
 package Package;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -55,7 +57,7 @@ public class Taller1 {
 							String Password = String.valueOf(scanner.nextLine());
 							
 							if(Contraseña.equals(Password)) {
-								for(int j = 0; i < Registros.length; j++) {
+								for(int j = 0; j < Registros.length; j++) {
 									if(Registros[j] !=null) {
 										String[] partes2 = Registros[j].split(";");
 										String ID2 = partes2[0];         
@@ -68,21 +70,118 @@ public class Taller1 {
 								}
 								
 									
+								int opcion; 
+								do {
+									System.out.println("Acceso correcto!");
+									System.out.println("");
+									System.out.println("Bienvenido" +" "+ User+"!");
+									System.out.println("");
+									System.out.println("Que deseas realizar?");
+									System.out.println("");
+									System.out.println("1) Registrar actividad.");
+									System.out.println("2) Modificar actividad.");
+									System.out.println("3) Eliminar actividad.");
+									System.out.println("4) Cambiar contraseña.");
+									System.out.println("5) Salir.");
+									
+									 opcion = Integer.valueOf(scanner.nextLine());
+									if(opcion == 1) {
+										
+									}
+									if(opcion == 2) {
+										int c = 0; // un contador
+										int[] lista = new int[300];
+										System.out.println("0) Regresar.");
+										for(int k = 0; k < Registros.length; k++) {
+											if(Registros[k]!=null) {
+												String[] partes2 = Registros[k].split(";");
+												String ID2 = partes2[0];         
+												String Fecha = partes2[1];
+												String Horas = partes2[2];
+												String Actividad = partes2[3];
+												
+												if(ID2.equals(User)) {
+													lista[c] = k;
+													c++;
+													
+													
+													System.out.println(c+")"+ " " + Registros[k]);
+													
+													
+													
+												}
+												
+											}
+										}
+										
+										int actividad = Integer.valueOf(scanner.nextLine());
+										int PRO = lista[actividad-1]; // posicion real original = PRO pdd: XD
+										System.out.println(Registros[PRO]);
+										String[] partes3 = Registros[PRO].split(";");
+										String ID3 = partes3[0];         
+										String Recuerdo = partes3[1];
+										String Tiempo = partes3[2];
+										String Situacion = partes3[3];
+										
 								
-								Scanner scanner2 = new Scanner(System.in);
-								System.out.println("Acceso correcto!");
-								System.out.println("");
-								System.out.println("Bienvenido" + User+"!");
-								System.out.println("");
-								System.out.println("Que deseas realizar?");
-								System.out.println("");
-								System.out.println("1) Registrar actividad.");
-								System.out.println("2) Modificar actividad.");
-								System.out.println("3) Eliminar actividad.");
-								System.out.println("4) Cambiar contraseña");
-								System.out.println("5) Salir.");
+										System.out.println("Que deseas modificar?");
+										System.out.println("");
+										System.out.println("0) Regesar.");
+										System.out.println("1) Fecha");
+										System.out.println("2) Duracion");
+										System.out.println("3) Tipo de actividad");
+										
+										
+										
+										
+										
+										
+										int selector = Integer.valueOf(scanner.nextLine());
+										
+										if(selector == 1) {
+											
+											try {
+												BufferedWriter fecha = new BufferedWriter(new FileWriter("Registros.txt",false));
+												System.out.println("0) Regresar.");
+												System.out.println("Ingrese nueva fecha(dia/mes/año): ");
+												String linea = String.valueOf(scanner.nextLine());
+												if(!linea.equals("0")) {
+													String [] partes4 = linea.split("/");
+													String dia = partes4[0];
+													String mes = partes4[1];
+													String año = partes4[2];
+													
+													String Respuesta = dia + "/" + mes + "/" + año;
+													
+													Registros[PRO] = ID3 + ";" + Respuesta + ";" + Tiempo + ";" + Situacion;
+													
+													
+													
+													
+												}
+												else {
+													fecha.close();
+													return;
+												}
+												for(int r = 0; r < Registros.length; r++) {
+													if(Registros[r] != null) {
+														fecha.write(Registros[r]);
+														fecha.newLine();
+														
+													}
+												}
+												fecha.close();
+											} catch (IOException e) {
+												
+												e.printStackTrace();
+											}
+											
+											
+										}
+									}
+									
+								}while(opcion !=5);
 								
-								String opcion = String.valueOf(scanner.nextLine());
 								
 							}
 							
